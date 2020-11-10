3eb7220b4597dcd075524ff294fa3678ecbe90ae
morel thiam <morel.thiam@live.fr>
2020-11-10 16:58:12 +0000

    first

---
+++ CRUDMVVMWPF/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDMVVMWPF.Models
+{
+    public class Student
+    {
+        public string Id { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public string BirthDay { get; set; }
+    }
+}
---
+++ CRUDMVVMWPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using CRUDMVVMWPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDMVVMWPF.ViewModels
+{
+    public class MainViewModel :BaseViewModel
+    {
+        #region Collection
+        private ObservableCollection<Student> _students;
+        public ObservableCollection<Student> Students
+        {
+            get { return _students; }
+            set { _students = value; OnPropertyChanged("Students"); }
+        }
+
+        #endregion
+
+        public MainViewModel()
+        {
+            Students = new ObservableCollection<Student>(GetDataStudent());
+        }
+
+        public List<Student> GetDataStudent()
+        {
+            List<Student> studentList = new List<Student>();
+            for (int i = 0; i < 10; i++)
+            {
+                studentList.Add(new Student {
+                    Id = Guid.NewGuid().ToString(),
+                    LastName = "Teacher Tree",
+                    FullName = "Susan Smith",
+                    BirthDay = DateTime.Now.Year.ToString()
+                });
+            }
+            return studentList;
+        }
+    }
+}
